05ffd523cd02502ef8fc8add5488e557116ca11a
itxmiska <gutierrezasa782@gmail.com>
2024-01-02 12:44:28 -0600

    add a question  button and a answer  and score in the html

---
+++ apps.js
@@ -0,0 +1,29 @@
+/*---------------------- Constants ------------------------*/
+const triviaQuestion = {
+  question: 'What color is the sky?',
+  answers: ['blue', 'green', 'yellow'],
+  correctAnswer: 'blue',
+}
+//render question 
+
+/*------------------- Variables (state) --------------------*/
+// let board, turn, winner, tie
+//set state for current question, answerClicked score
+let currQuestion, answerClicked, score
+
+/*--------------- Cached Element References ----------------*/
+const 
+const questionbutton = document.getElementById('question-button')
+
+/*---------------------- Event Listeners -------------------*/
+// reference TTT line 22-24 for implamenting event listeners to trivia question answers
+
+//add event listeners answers after iterating thru them with a for each method
+/*----------------------- Functions ------------------------*/
+// add init function
+// use forloop or forEach loop to iterate thru the questions
+init ()
+
+function init() {
+
+}
