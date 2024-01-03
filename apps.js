2dc2470f65c5ad4b7f3d7d443a8aa289fb23b91f
itxmiska <gutierrezasa782@gmail.com>
2024-01-03 06:10:46 -0600

    add a init fuction and renderstateofgame fuction and a handleanswer fuction

--- apps.js
+++ apps.js
@@ -1,20 +1,34 @@
 /*---------------------- Constants ------------------------*/
 const triviaQuestion = {
   question: 'What color is the sky?',
-  answers: ['blue', 'green', 'yellow'],
-  correctAnswer: 'blue',
+  answers: [
+    {
+      answer:'blue', 
+      isAnswer: true
+    },
+    {
+      answer:'green',
+      isAnswer: false
+    },
+    {
+      answer:'yellow',
+      isAnswer: false
+  }
+  ],
+  
 }
 //render question 
 
 /*------------------- Variables (state) --------------------*/
 // let board, turn, winner, tie
 //set state for current question, answerClicked score
-let currQuestion, answerClicked, score
+let currQuestionIndex = 0
+let score = 0
 
 /*--------------- Cached Element References ----------------*/
-const 
-const questionbutton = document.getElementById('question-button')
-
+const questionElement = document.getElementById('question')
+const answersElement = document.getElementById('anwsers')
+const scoreElement = document.getElementById('score')
 /*---------------------- Event Listeners -------------------*/
 // reference TTT line 22-24 for implamenting event listeners to trivia question answers
 
@@ -24,6 +38,36 @@ const questionbutton = document.getElementById('question-button')
 // use forloop or forEach loop to iterate thru the questions
 init ()
 
-function init() {
+function initGame() {
+  currQuestionIndex = 0
+  Score = 0
+  renderGame()
+}
 
+function renderStateOfGame() {
+  if (currQuestionIndex < questions.length) {
+    const currentQuestion = questions[currQuestionIndex]
+    questionElement.textContent = currentQuestion.question
+    answersElement.innerHTML = ""
+    currentQuestion.answers.forEach((answer, index) => {
+      const button = document.createElement('button')
+      button.textContent = answer
+      button.addEventListener('click', () => handleAnswer(index))
+      answersElement,appendChild(button)
+    })
+    scoreElement.textContent = `Score ${score}`
+  } else {
+    endGame()
+  }
+}
+
+function handleAnswer(answerIndex) {
+  const currentQuestion = questions[currQuestionIndex]
+  
+
+  if (currentQuestion.answers[answerIndex].correct) {
+    score++
+  }
+  currQuestionIndex++
+  renderGame()
 }
